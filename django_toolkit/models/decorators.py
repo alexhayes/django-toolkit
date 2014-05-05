b2a8092967abee7e7728fc164851d5671cc37f7d
Alex Hayes <alex.hayes@roi.com.au>
2014-05-05 14:09:13 +1000

    Changed django_toolkit.models.decorators.refresh to not reassign self

--- django_toolkit/models/decorators.py
+++ django_toolkit/models/decorators.py
@@ -29,10 +29,18 @@ def refresh(func):
         
     This behavior is useful in a distributed system, such as celery, where 
     "asserting the world is the responsibility of the task" - see http://celery.readthedocs.org/en/latest/userguide/tasks.html?highlight=model#state
+    
+    Note that the refresh of the model uses the approach outlined in https://github.com/planop/django/blob/ticket_901/django/db/models/base.py#L1012
+    which was discovered after from https://code.djangoproject.com/ticket/901#comment:29
+    which is a Django ticket which discusses a specific method 'refresh' on a 
+    model.
     """
-
+    
     @wraps(func)
     def inner(self, *args, **kwargs):
-        self = self.__class__.objects.get(pk=self.pk)
+        # Refresh the model instance - see https://github.com/planop/django/blob/ticket_901/django/db/models/base.py#L1012
+        new_self = self.__class__._base_manager.using(self._state.db).get(pk=self.pk)
+        for f in self.__class__._meta.fields:
+            setattr(self, f.name, getattr(new_self, f.name))
         return func(self, *args, **kwargs)
     return inner
